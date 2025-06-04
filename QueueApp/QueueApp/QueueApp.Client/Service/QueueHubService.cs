34862202b976c47f5a783367cd32a6d6f5c2a023
haranskyi-ko <haranskyi_ko-2024@knuba.edu.ua>
2025-06-05 02:52:05 +0300

    Added client pages structure

---
+++ QueueApp/QueueApp/QueueApp.Client/Service/QueueHubService.cs
@@ -0,0 +1,25 @@
+﻿// Services/QueueHubService.cs
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.Components;
+
+namespace QueueApp.Client.Services;
+
+public sealed class QueueHubService
+{
+    private readonly NavigationManager _nav;
+
+    public HubConnection Connection { get; }
+
+    public QueueHubService(NavigationManager nav)
+    {
+        _nav = nav;
+
+        Connection = new HubConnectionBuilder()
+            .WithUrl(_nav.ToAbsoluteUri("/queuehub"))
+            .WithAutomaticReconnect()
+            .Build();
+    }
+
+    public Task StartAsync() => Connection.StartAsync();
+    public Task StopAsync() => Connection.StopAsync();
+}
